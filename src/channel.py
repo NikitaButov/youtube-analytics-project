7cdea67721d056ae657fe4b08a8fce148840445c
Nikita Butov <butov3084@gmail.com>
2023-08-10 09:18:57 +0500

    Обновил channel.py и переделал тест Добавил фаил json

--- src/channel.py
+++ src/channel.py
@@ -11,11 +11,48 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.title = ""
+        self.description = ""
+        self.url = ""
+        self.subscriber_count = 0
+        self.video_count = 0
+        self.view_count = 0
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        url = f"https://www.googleapis.com/youtube/v3/channels?part=snippet&id={self.channel_id}&key={api_key}"
+        self._fetch_channel_info()
+
+    def _fetch_channel_info(self) -> None:
+        """Заполняет атрибуты экземпляра данными о канале."""
+        url = f"https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id={self.channel_id}&key={api_key}"
         response = requests.get(url)
         data = response.json()
 
-        print(json.dumps(data, indent=2, ensure_ascii=False))
+        try:
+            channel_info = data["items"][0]
+        except KeyError:
+            raise KeyError('Ошибка при получении данных о канале')
+        self.title = channel_info["snippet"]["title"]
+        self.description = channel_info["snippet"]["description"]
+        self.url = f"https://www.youtube.com/channel/{self.channel_id}"
+        self.subscriber_count = int(channel_info["statistics"]["subscriberCount"])
+        self.video_count = int(channel_info["statistics"]["videoCount"])
+        self.view_count = int(channel_info["statistics"]["viewCount"])
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube API."""
+        return requests.Session()
+
+    def to_json(self, file_path: str) -> None:
+        """Сохраняет значения атрибутов экземпляра Channel в файл в формате JSON."""
+        data = {
+            "channelId": self.channel_id,
+            "title": self.title,
+            "description": self.description,
+            "url": self.url,
+            "subscriberCount": self.subscriber_count,
+            "videoCount": self.video_count,
+            "viewCount": self.view_count
+        }
+
+        with open(file_path, "w") as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,14 +1,34 @@
+import json
+
 import pytest
+import os
+
+import requests
 
 from src.channel import Channel
 
-
-@pytest.fixture
-def channel():
-    channel_id = "1"
-    return Channel(channel_id)
+api_key: str = os.getenv('y_api_kei')
 
 
-def test_print_info(channel):
-    channel.__init__("UC-OVMPlMA3-YCIeg4z5z23A")
-    channel.print_info()
+def test_to_json(tmp_path):
+    file_path = tmp_path / "channel.json"
+
+    channel = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+    channel.to_json(file_path)
+
+    expected_data = {
+        "channelId": "UC-OVMPlMA3-YCIeg4z5z23A",
+        "title": "",
+        "description": "",
+        "url": "",
+        "subscriberCount": 0,
+        "videoCount": 0,
+        "viewCount": 0
+    }
+
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+        assert data != expected_data
+
+
